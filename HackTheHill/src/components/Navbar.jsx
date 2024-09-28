578c1b005ad82d1c028cf149b843fe652e3fcb54
Yushika Jhundoo <pjhun035@uottawa.ca>
2024-09-28 03:28:09 -0400

    modified:   HackTheHill/src/components/Navbar.jsx

--- HackTheHill/src/components/Navbar.jsx
+++ HackTheHill/src/components/Navbar.jsx
@@ -1,28 +1,19 @@
-// src/components/Navbar.jsx
+// src/components/NavBar.jsx
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './Navbar.css'; // We'll create this CSS file next
 
-const Navbar = () => {
-  return (
-    <nav className="navbar">
-      <h2 className="navbar-logo">Language Learner</h2>
-      <ul className="navbar-links">
-        <li>
-          <Link to="/music">Music</Link>
-        </li>
-        <li>
-          <Link to="/books">Books</Link>
-        </li>
-        <li>
-          <Link to="/movies">Movies</Link>
-        </li>
-        <li>
-          <Link to="/chatbot">Chatbot</Link>
-        </li>
-      </ul>
-    </nav>
-  );
+const NavBar = () => {
+    return (
+        <nav>
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/music">Music</Link></li>
+                <li><Link to="/books">Books</Link></li>
+                <li><Link to="/movies">Movies</Link></li>
+                <li><Link to="/chatbot">Chatbot</Link></li>
+            </ul>
+        </nav>
+    );
 };
 
-export default Navbar;
+export default NavBar;
