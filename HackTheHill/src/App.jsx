f950142254edd2b45c0cf5d2ea9f6f7ae752cec3
Yushika Jhundoo <pjhun035@uottawa.ca>
2024-09-28 03:43:01 -0400

    create books.jsx

--- HackTheHill/src/App.jsx
+++ HackTheHill/src/App.jsx
@@ -1,40 +1,39 @@
 // src/App.jsx
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/NavBar';
+import Navbar from './components/Navbar';
 import Music from './components/Music';
-import Books from './components/Books';
+import Books from './components/Books'; 
 import Movies from './components/Movies';
 import Chatbot from './components/Chatbot';
 import './App.css'; 
 
 const App = () => {
-  return (
-    <Router>
-      <div>
-        <Navbar /> {/* Include the Navbar at the top */}
-        <div className="content">
-          <Routes>
-            <Route path="/music" element={<Music />} />
-            <Route path="/books" element={<Books />} />
-            <Route path="/movies" element={<Movies />} />
-            <Route path="/chatbot" element={<Chatbot />} />
-            <Route path="/" element={<Home />} /> {/* Default Home component */}
-          </Routes>
-        </div>
-      </div>
-    </Router>
-  );
+    return (
+        <Router>
+            <div>
+                <Navbar />
+                <div className="content">
+                    <Routes>
+                        <Route path="/music" element={<Music />} />
+                        <Route path="/books" element={<Books />} />
+                        <Route path="/movies" element={<Movies />} />
+                        <Route path="/chatbot" element={<Chatbot />} />
+                        <Route path="/" element={<Home />} />
+                    </Routes>
+                </div>
+            </div>
+        </Router>
+    );
 };
 
-// Optional: Create a Home component
 const Home = () => {
-  return (
-    <div>
-      <h2>Welcome to Language Learner!</h2>
-      <p>Select a category from the navigation bar to get started.</p>
-    </div>
-  );
+    return (
+        <div>
+            <h2>Welcome to Language Learner!</h2>
+            <p>Select a category from the navigation bar to get started.</p>
+        </div>
+    );
 };
 
 export default App;
---
+++ HackTheHill/src/components/Books.jsx
@@ -0,0 +1,8 @@
+// src/components/Books.jsx
+import React from 'react';
+
+const Books = () => {
+    return <div>This is the Books component.</div>;
+};
+
+export default Books;
